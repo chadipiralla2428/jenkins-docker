3042a320c5bab22c3d43880fcf18bdebad7b09e2
chadipiralla2428 <schadipiralla@gmail.com>
2023-03-22 08:36:29 +0530

    docker first commit

---
+++ src/test/java/com/example/jenkinsdockerdemo/JenkinsdockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdockerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
